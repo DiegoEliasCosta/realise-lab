a662e63d8a84b24b1c93c114e9f05447461440b0
MohamedBilelBesbes <mohamedbilel.besbes@supcom.tn>
2024-06-09 13:15:51 -0400

    fixing responsiveness & news in main page

--- src/components/NewsOnHomePage.jsx
+++ src/components/NewsOnHomePage.jsx
@@ -10,9 +10,9 @@ const PartnersMainPage = () => {
   return (
       <div className="bg-gray-100/5">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div className="mx-auto my-7 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {partners.map((partner) => (
-              <article key={partner.name} className="flex max-w-xl flex-col items-start justify-between">
+              <article key={partner.name} className="flex max-w-xl flex-col items-start justify-between hover:scale-105 duration-200">
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                     <a href={partner.link} target="_blank" rel="noopener noreferrer">
